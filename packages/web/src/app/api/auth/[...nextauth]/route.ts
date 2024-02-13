1f595543a73f7fbcfbd80796e514fc533cf72ee2
Felipe <felipesullivan50@gmail.com>
2024-02-13 19:07:52 -0300

    config: configuração autenticação com facebook

--- packages/web/src/app/api/auth/[...nextauth]/route.ts
+++ packages/web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,31 @@
 import NextAhth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+// import FacebookProvider from 'next-auth/providers/facebook'
 
-const clientId = process.env.NEXT_PUBLIC_CLIENT_ID ?? ''
-const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET ?? ''
+const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? ''
+const googleClientSecret = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? ''
+
+/**
+ * autenticação com facebook requer finalziação do mvp para incluir politica de privacidades
+ */
+
+// const facebookClientId = process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID ?? ''
+// const facebookClientSecret =
+//   process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET ?? ''
+
 const secret = process.env.NEXT_PUBLIC_SECRET ?? ''
 
 // @ts-expect-error: in build process, AuthOptions, by next-auth imports don't work and show an error message about AuthOptions itsn't incompatible with type never
 export const authOptions: never = {
   providers: [
     GoogleProvider({
-      clientId,
-      clientSecret,
+      clientId: googleClientId,
+      clientSecret: googleClientSecret,
     }),
+    //   FacebookProvider({
+    //     clientId: facebookClientId,
+    //     clientSecret: facebookClientSecret,
+    //   }),
   ],
   secret,
 }
